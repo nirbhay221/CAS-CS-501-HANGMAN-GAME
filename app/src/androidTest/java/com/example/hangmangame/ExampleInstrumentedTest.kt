045409fd0ce0afd157bcdc58851c2205486278ed
IAmRhy31 <rhythmsomaiya31@gmail.com>
2023-10-13 21:52:53 -0400

    Added ITs

--- app/src/androidTest/java/com/example/hangmangame/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/hangmangame/ExampleInstrumentedTest.kt
@@ -1,12 +1,18 @@
 package com.example.hangmangame
 
-import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.core.app.ActivityScenario
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.test.platform.app.InstrumentationRegistry
+import android.content.res.Configuration
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
-
 import org.junit.Assert.*
+
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -21,4 +27,43 @@ class ExampleInstrumentedTest {
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.example.hangmangame", appContext.packageName)
     }
+
+    @Test
+    fun completeWordAndWinTest() {
+
+        val scenario = ActivityScenario.launch(MainActivity2::class.java)
+
+        Espresso.onView(ViewMatchers.withId(R.id.button))
+            .perform(ViewActions.click())
+
+        val letters = listOf('A', 'P', 'P', 'L', 'E')
+
+        for (letter in letters) {
+            Espresso.onView(ViewMatchers.withText(letter.toString()))
+                .perform(ViewActions.click())
+        }
+
+        Thread.sleep(3000)
+
+        scenario.close()
+    }
+
+    @Test
+    fun hintButtonVisibilityTest() {
+        val context = InstrumentationRegistry.getInstrumentation().targetContext
+
+        context.resources.configuration.orientation = Configuration.ORIENTATION_PORTRAIT
+        val scenarioPortrait = ActivityScenario.launch(MainActivity2::class.java)
+
+        Espresso.onView(ViewMatchers.withId(R.id.button))
+            .perform(ViewActions.click())
+        Espresso.onView(ViewMatchers.withId(R.id.hintButton))
+            .check(ViewAssertions.doesNotExist())
+
+        Thread.sleep(3000)
+        scenarioPortrait.close()
+    }
+
+
+
 }
